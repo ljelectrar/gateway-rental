7a15a6857fcca11d7fd8c1aa401c060f468f939b
ljelectrar <ljelectrar@gmail.com>
2024-02-12 18:16:24 -0300

    Model Initial Setup

--- src/main/java/com/ljelectrar/model/PatchUserRequest.java
+++ src/main/java/com/ljelectrar/model/PatchUserRequest.java
@@ -1,5 +1,36 @@
 package com.ljelectrar.model;
 
-public class PatchUserRequest {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+public class PatchUserRequest {
+	@JsonProperty("first_name")
+	private String firstName;
+
+	@JsonProperty("last_name")
+	private String lastName;
+	private String email;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
